ff1d8cc0c2df71df948edf064f24287eddd6ece9
Jose-Maria-Gutierrez <104695943+Jose-Maria-Gutierrez@users.noreply.github.com>
2022-05-12 11:55:00 -0300

    Delete dbo.Paciente.sql

--- dbo.Paciente.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Paciente] (
-    [Id]        INT          IDENTITY (1, 1) NOT NULL,
-    [Nombre]    VARCHAR (50) NULL,
-    [Apellido]  VARCHAR (50) NULL,
-    [DNI]       VARCHAR (50) NULL,
-    [Direccion] VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
