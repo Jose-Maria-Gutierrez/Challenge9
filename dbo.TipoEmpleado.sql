d2cb7f96a0c7f1e443a056d267bc4f44d31d8b55
Jose-Maria-Gutierrez <104695943+Jose-Maria-Gutierrez@users.noreply.github.com>
2022-05-12 11:55:07 -0300

    Delete dbo.TipoEmpleado.sql

--- dbo.TipoEmpleado.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TipoEmpleado] (
-    [Id]          INT          IDENTITY (1, 1) NOT NULL,
-    [Descripcion] VARCHAR (50) NULL,
-    [FechaBaja]   DATETIME     NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
