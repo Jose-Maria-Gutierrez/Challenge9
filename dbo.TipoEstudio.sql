dd91eb493da27776e912a35ea40cf3d3819479d9
Jose-Maria-Gutierrez <104695943+Jose-Maria-Gutierrez@users.noreply.github.com>
2022-05-12 11:55:13 -0300

    Delete dbo.TipoEstudio.sql

--- dbo.TipoEstudio.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TipoEstudio] (
-    [IdEstudio]   INT          IDENTITY (1, 1) NOT NULL,
-    [Descripcion] VARCHAR (50) NULL,
-    [FechaBaja]   DATETIME     NULL,
-    PRIMARY KEY CLUSTERED ([IdEstudio] ASC)
-);
-
