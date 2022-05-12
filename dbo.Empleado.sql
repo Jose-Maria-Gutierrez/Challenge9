d6596905a66794364f4961acd58b42cfb6444310
Jose-Maria-Gutierrez <104695943+Jose-Maria-Gutierrez@users.noreply.github.com>
2022-05-12 11:49:43 -0300

    ClinicaTablas

---
+++ dbo.Empleado.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Empleado] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [TipoEmpleadoId] INT          NULL,
+    [Nombre]         VARCHAR (50) NULL,
+    [Apellido]       VARCHAR (50) NULL,
+    [DNI]            VARCHAR (50) NULL,
+    [Legajo]         VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([TipoEmpleadoId]) REFERENCES [dbo].[TipoEmpleado] ([Id])
+);
+
